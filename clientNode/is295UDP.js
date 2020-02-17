e07feec583965287960aac7a456028e8191b2bc0
Christopher Coballes <cobecoballes@gmail.com>
2020-02-18 00:14:41 +0800

    Update is295UDP.js

--- clientNode/is295UDP.js
+++ clientNode/is295UDP.js
@@ -29,7 +29,7 @@ app.use(express.static(__dirname));
 var IS295data = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'robook1234',
+    password: 'robookxxxx',
     database: 'dcmsDB;'
 });	
 
